323865a590066ee1ad6123903f362b70328973e1
MrPineapplehead <mrpineapplehead@192-168-1-5.tpgi.com.au>
2015-05-02 14:06:03 +1000

    updated test

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -5,5 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
-
+	@Test
+	public void testDebitWithSufficientFunds() {
+		BankAccount account = new BankAccount(100);
+		double amount = account.debit(5);
+		Assert.assertEquals(95.0, amount);
+	}
 }
